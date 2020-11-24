02c77a8c2b467a80aaadff0670f4ed25f2c62c10
Anh Minh Tran <anhminhtran235@gmail.com>
2020-11-24 14:06:06 -0800

    minor: protect variable from minification

--- routing/app.js
+++ routing/app.js
@@ -2,18 +2,21 @@
 
 const app = angular.module('routingApp', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/main', {
-      templateUrl: 'pages/main.html',
-      controller: 'mainController',
-    })
-    .when('/first', {
-      templateUrl: 'pages/first.html',
-      controller: 'firstController',
-    })
-    .when('/second', {
-      templateUrl: 'pages/second.html',
-      controller: 'secondController',
-    });
-});
+app.config([
+  '$routeProvider',
+  function ($routeProvider) {
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController',
+      })
+      .when('/first', {
+        templateUrl: 'pages/first.html',
+        controller: 'firstController',
+      })
+      .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController',
+      });
+  },
+]);
